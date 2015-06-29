337650e78de0877e8dd7c0cf34657bc88cde9cf9
sduxzz <825502451@qq.com>
2015-06-29 23:16:10 +0800

    delete ErrorType enum

--- lang4-util/src/main/java/lang4/constants/StatusType.java
+++ lang4-util/src/main/java/lang4/constants/StatusType.java
@@ -14,41 +14,52 @@ public enum StatusType {
     /**
      * request successful
      */
-     OK(200),
+    OK(200, "ok"),
 
     /**
      * system error
      */
-     SYS_ERROR(100),
+    SYS_ERROR(100, "system error"),
 
     /**
      * parameters are invalid
      */
-     PARAM_ERROR(300),
+    PARAM_ERROR(300, "request parameters are invalid"),
 
     /**
      * no authority error
      */
-     AUTH_ERROR(400),
+    AUTH_ERROR(400, "you don't has this authority"),
+
+    /**
+     * no login
+     */
+    NO_LOGIN_ERROR(401, "hasn't login yet"),
 
     /**
      * forbidden by system
      */
-     FORBID_ERROR(500),
+    FORBID_ERROR(500, "forbidden by system"),
 
     /**
      * request timeout
      */
-     TIMEOUT_ERROR(600);
+    TIMEOUT_ERROR(600, "request timeout");
 
-     private int code;
-     
-     private StatusType(int code) {
-         this.code = code;
-     }
+    private int code;
+    private String message;
+
+    private StatusType(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 
     public int getCode() {
         return code;
+    }
+
+    public String getMessage() {
+        return message;
     }
 
     /**
