8f4931b44716c17e3e785f2f624e6710d139fdec
sduxzz <825502451@qq.com>
2015-09-24 19:46:43 +0800

    delete unused comment

--- lang4-util/src/main/java/lang4/constants/StatusType.java
+++ lang4-util/src/main/java/lang4/constants/StatusType.java
@@ -16,50 +16,26 @@ public enum StatusType {
      */
     OK(200, "ok"),
 
-    /**
-     * system error
-     */
     SYS_ERROR(100, "system error"),
 
-    /**
-     * parameters are invalid
-     */
     PARAM_ERROR(300, "request parameters are invalid"),
 
-    /**
-     * no authority error
-     */
     AUTH_ERROR(400, "you don't has this authority"),
 
-    /**
-     * no login
-     */
     NO_LOGIN_ERROR(401, "hasn't login yet"),
 
-    /**
-     * forbidden by system
-     */
+    WRONG_USER_OR_PWD(402, "用户不存在或者密码错误"),
+
     FORBID_ERROR(500, "forbidden by system"),
 
-    /**
-     * request timeout
-     */
     TIMEOUT_ERROR(600, "request timeout");
 
-    private int code;
-    private String message;
+    public int code;
+    public String message;
 
     private StatusType(int code, String message) {
         this.code = code;
         this.message = message;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
     }
 
     /**
